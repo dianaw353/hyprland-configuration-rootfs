090927300c5bbb8c8f08a16aa836ac65bf50185f
Botan-Yuukan <botanyuukan@gmail.com>
2023-09-17 14:41:30 -0600

    added more configs for dunst, wezterm, hypr, and more

---
+++ home/botan/.config/wezterm/wezterm.lua
@@ -0,0 +1,65 @@
+local wezterm = require("wezterm")
+
+local function font_with_fallback(name, params)
+	local names = { name, "Apple Color Emoji", "azuki_font" }
+	return wezterm.font_with_fallback(names, params)
+end
+
+local font_name = "JetBrainsMonoNL NF"
+
+return {
+	-- OpenGL for GPU acceleration, Software for CPU
+	front_end = "OpenGL",
+
+	color_scheme = 'Catppuccin Mocha',
+
+	-- Font config
+	font = font_with_fallback(font_name),
+	font_rules = {
+		{
+			italic = true,
+			font = font_with_fallback(font_name, { italic = true }),
+		},
+		{
+			italic = false,
+			font = font_with_fallback(font_name, { bold = true }),
+		},
+		{
+			intensity = "Bold",
+			font = font_with_fallback(font_name, { bold = true }),
+		},
+	},
+	warn_about_missing_glyphs = false,
+	font_size = 10,
+	line_height = 1.0,
+	dpi = 96.0,
+
+	-- Cursor style
+	default_cursor_style = "BlinkingUnderline",
+
+	-- X11
+	enable_wayland = true,
+
+	-- Aesthetic Night Colorscheme
+	bold_brightens_ansi_colors = true,
+	-- Padding
+	window_padding = {
+		left = 25,
+		right = 25,
+		top = 25,
+		bottom = 25,
+	},
+
+	-- Tab Bar
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	show_tab_index_in_tab_bar = false,
+	tab_bar_at_bottom = true,
+
+	-- General
+	automatically_reload_config = true,
+	inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 },
+	window_background_opacity = 0.5,
+	window_close_confirmation = "NeverPrompt",
+  window_frame = { active_titlebar_bg = "#45475a", font = font_with_fallback(font_name, { bold = true }) },
+}
