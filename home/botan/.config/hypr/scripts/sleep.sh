baa476ae05d68906e77acd3b5d0740d2b89ccef2
Botan Yuukan <65094216+Botan-Yuukan@users.noreply.github.com>
2023-09-19 03:44:57 +0000

    removed some unnecarry configs, added rofi binds, and sway idle include wallpaper

--- home/botan/.config/hypr/scripts/sleep.sh
+++ home/botan/.config/hypr/scripts/sleep.sh
@@ -1,4 +1,4 @@
-swayidle -w timeout 300 'swaylock -f -c 000000' \
+swayidle -w timeout 300 'swaylock -f -c 000000 -i ~/.wallpaper/Untitled.jpg' \
             timeout 400 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
-            before-sleep 'swaylock -f -c 000000' 
+            before-sleep 'swaylock -f -c 000000 -i ~/.wallpaper/Untitled.jpg'
