fd622a4a192b1551ea33a70882d4bbe862777dce
Dianaw <65094216+dianaw353@users.noreply.github.com>
2023-12-11 04:34:28 +0000

    add network.sh

---
+++ home/diana/.config/hypr/settings/network.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Function to connect to the network
+connect_to_network() {
+    # Get the list of WiFi networks
+    echo "scanning for networks... please wait"
+    ssid=$(nmcli --fields "SSID" device wifi list | grep -v -- '--' | gum filter --placeholder "Please select your WIFI newtwork...")
+
+    # Trim leading and trailing spaces from the SSID
+    ssid=$(echo "$ssid" | xargs)
+
+    while true; do
+        # Prompt for the network password
+        echo "Please enter your password for $ssid:"
+        password=$(gum input --password --placeholder "Please type in password for $ssid")
+
+        # Try to connect to the network
+        if nmcli device wifi connect "$ssid" password "$password"; then
+            echo "Successfully connected to $ssid!"
+            break
+        else
+            echo "Failed to connect to $ssid. Please check your password and try again."
+            echo "Do you want to try again? (yes/no)"
+            read answer
+            if [ "$answer" != "${answer#[Nn]}" ] ;then
+                break
+            fi
+        fi
+    done
+}
+
+connect_to_network
+
+
