c5971a474dac01548e44ee420d6c2c0c0e6cd930
Dianaw <65094216+dianaw353@users.noreply.github.com>
2023-12-13 00:19:42 +0000

    improve custom backlight

--- home/diana/.config/waybar/scripts/brightness.sh
+++ home/diana/.config/waybar/scripts/brightness.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-brightness=$(brillo -G)
-brightness_rounded=$(printf "%.0f" $brightness)
-echo "{\"text\":\"$brightness_rounded%\"}"
+
+# Function to adjust brightness
+adjust_brightness() {
+    local operation=$1
+    declare -A operations=( ["increase"]="-A" ["decrease"]="-U" )
+    brillo -u 15000 ${operations[$operation]} 2
+}
+
+# Adjust the brightness based on the argument
+case "$1" in
+    "increase"|"decrease") adjust_brightness $1 ;;
+esac
+
+# Get the current brightness and convert it to an integer
+brightness=$(printf "%.0f" "$(brillo -G)")
+
+# Define the icons
+icons=("󰃞" "󰃟" "󰃝" "󰃠")
+
+# Calculate the index based on the brightness percentage
+index=$(( brightness / 25 ))
+
+# Correct the index if brightness is 100
+(( brightness == 100 )) && index=3
+
+# Print the icon and the brightness percentage
+printf '{"text":"%s %s%%"}\n' "${icons[$index]}" "$brightness"
 
