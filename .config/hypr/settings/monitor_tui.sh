9c8220243b34bba025aa3a9cff87952a05dae651
Dianaw <65094216+dianaw353@users.noreply.github.com>
2023-12-30 19:30:35 +0000

    update

--- .config/hypr/settings/monitor_tui.sh
+++ .config/hypr/settings/monitor_tui.sh
@@ -2,10 +2,62 @@
 
 figlet "monitor"
 
-# Function to backup the current monitor configuration
+# Function to backup the current monitor configuration with date and time
 backup_monitor_config() {
     echo "Backing up the current monitor configuration..."
-    cp ~/.config/hypr/conf/monitors/default.conf ~/.config/hypr/conf/monitors/backup
+
+    # Specify the backup folder
+    backup_folder=~/.config/hypr/conf/monitors/backup/
+
+    # Create the backup folder if it doesn't exist
+    mkdir -p "$backup_folder"
+
+    # Adding date and time to the backup file name
+    backup_file="$backup_folder"backup_$(date +"%Y%m%d%H%M%S").conf
+
+    cp ~/.config/hypr/conf/monitors/default.conf "$backup_file"
+
+    echo "Backup saved to $backup_file"
+}
+
+# Function to restore the backup configuration
+restore_backup_config() {
+    # Prompt user to restore configuration
+    gum confirm "Do you want to restore a backup configuration?"
+
+    if [ $? -eq 0 ]; then
+        # Specify the backup folder
+        backup_folder=~/.config/hypr/conf/monitors/backup/
+
+        # List available backup files
+        available_files=("$backup_folder"*)
+        if [ ${#available_files[@]} -eq 0 ]; then
+            echo "No backup files found in $backup_folder. Skipping restore."
+            return
+        fi
+
+        # Prompt user to choose a file to restore
+        selected_backup_file=$(gum choose --height 15 "${available_files[@]}")
+
+        # Check if the selected file exists
+        if [ -f "$selected_backup_file" ]; then
+            cp "$selected_backup_file" ~/.config/hypr/conf/monitors/default.conf
+            echo "Configuration restored successfully from $selected_backup_file."
+
+            # Prompt user to continue configuration or exit
+            gum confirm "Do you want to continue the configuration or exit the script?"
+
+            if [ $? -eq 0 ]; then
+                return
+            else
+                exit
+            fi
+        else
+            echo "Backup file $selected_backup_file not found. Skipping restore."
+        fi
+    else
+        echo "Skipping restore."
+    fi
 }
 
 # Function to get information about connected monitors
@@ -42,11 +94,6 @@ configure_monitors() {
     refreshrate=$(wlr-randr | awk -v selected_monitor="$selected_monitor" '/^'"$selected_monitor"'/,/^$/ {print $0}' | grep -oP '\d{1,3}\.\d{6}' | awk '{printf("%.0f\n", $1)}' | sort -u -n | tac | gum choose --height 15)
     echo "You chose the refresh rate to be: $refreshrate"
 
-    # Select position
-    echo "Please select the position of the display relative to other displays"
-    position=$(gum choose --height 15 "auto" "right" "left")
-    echo "Setting position to: $position"
-
     # Select scale
     scaling=""
     while ! [[ "$scaling" =~ ^[0-9]+(\.[0-9]+)?$ ]] || (( $(awk -v x=$scaling -v y=3 'BEGIN {print (x > y)}') )); do
@@ -56,44 +103,110 @@ configure_monitors() {
     done
     echo "Setting scale to: $scaling"
 
-    # Extract x and y resolutions
-    x_resolution=$(echo "$screenres" | cut -d'x' -f1)
-    y_resolution=$(echo "$screenres" | cut -d'x' -f2)
-    
-    case $position in
-    "auto")
-        position_res="auto"
-        ;;
-    "right")
-        position_res="${x_resolution}x0"
-        ;;
-    "left")
-        position_res="-${x_resolution}x0"
-        ;;
-    *)
-        echo "Invalid position. Defaulting to top-right."
-        position_res="${x_resolution}x0"
-        ;;
-    esac
-
-     # Replace or add the line in the default.conf file
+         # Replace or add the line in the default.conf file
     if grep -q "monitor=$selected_monitor" ~/.config/hypr/conf/monitors/default.conf; then
-        sed -i "s/monitor=$selected_monitor,.*/monitor=$selected_monitor,$screenres@$refreshrate,$position_res,$scaling/g" ~/.config/hypr/conf/monitors/default.conf
+        sed -i "s/monitor=$selected_monitor,.*/monitor=$selected_monitor,$screenres@$refreshrate,auto,$scaling/g" ~/.config/hypr/conf/monitors/default.conf
     else
         echo "monitor=$selected_monitor,$screenres@$refreshrate,auto,$scaling" >> ~/.config/hypr/conf/monitors/default.conf
     fi
 }
 
-# Function to confirm or restore the monitor configuration
+# Function to configure advanced settings
+configure_advanced_settings() {
+    gum confirm "Do you want to configure advanced settings for monitors?"
+
+    if [ $? -eq 0 ]; then
+        gum confirm "Do you want to enable 10-bit support?"
+
+        if [ $? -eq 0 ]; then
+            echo "Enabling 10-bit support..."
+            # Add your code here to enable 10-bit support
+        else
+            echo "10-bit support will not be enabled."
+        fi
+
+        monitors=$(echo "$monitors" | awk '{print $1}' | paste -sd ' ')
+
+        # Check if there is more than one monitor to mirror
+        if [ $(echo "$monitors" | wc -l) -gt 1 ]; then
+            gum confirm "Do you want to set up a mirrored display?"
+
+            if [ $? -eq 0 ]; then
+                # Prompt to select the main monitor for mirroring
+                echo "Choose the main monitor for mirroring:"
+                main_monitor=$(gum choose --height 15 $monitors)
+                echo "Selected main monitor for mirroring: $main_monitor"
+
+                # Now, prompt to select the monitor to mirror
+                available_monitors=$(echo "$monitors" | grep -v "$main_monitor" | awk '{print $1}' | paste -sd ' ')
+                echo "Choose the monitor to mirror:"
+                mirror_monitor=$(gum choose --height 15 $available_monitors)
+                echo "Selected monitor to mirror: $mirror_monitor"
+
+                # Add your code here to set up mirror display using $main_monitor and $mirror_monitor variables
+            else
+                echo "Mirror display will not be set up."
+            fi
+        else
+            echo "Only one monitor available. Skipping mirroring configuration."
+        fi
+
+        gum confirm "Do you want to rotate a monitor?"
+
+        if [ $? -eq 0 ]; then
+            echo "Choose the rotation option:"
+            rotation_option=$(gum choose --height 15 "normal (no transforms) -> 0" "90 degrees -> 1" "180 degrees -> 2" "270 degrees -> 3" "flipped -> 4" "flipped + 90 degrees -> 5" "flipped + 180 degrees -> 6" "flipped + 270 degrees -> 7")
+            echo "Selected rotation option: $rotation_option"
+            # Add your code here to apply the selected rotation option
+        else
+            echo "No rotation will be applied."
+        fi
+
+        gum confirm "Do you want to control VRR (Adaptive Sync) of your monitors? 0 - off, 1 - on, 2 - fullscreen only"
+        vrr_option=$(gum choose --height 15 "VRR off -> 0" "VRR on -> 1" "VRR fullscreen only -> 2")
+
+        case $vrr_option in
+            0)
+                echo "VRR (Adaptive Sync) will be turned off."
+                # Add your code here to handle VRR off
+                ;;
+            1)
+                echo "VRR (Adaptive Sync) will be turned on."
+                # Add your code here to handle VRR on
+                ;;
+            2)
+                echo "VRR (Adaptive Sync) will be set to fullscreen only."
+                # Add your code here to handle VRR fullscreen only
+                ;;
+            *)
+                echo "Invalid option. Skipping VRR configuration."
+                ;;
+        esac
+
+    else
+        echo "Skipping advanced settings configuration."
+    fi
+}
+
+# Function to confirm or restore the monitor configuration and configure advanced settings
 confirm_or_restore_config() {
     gum confirm "Do you want to use the new monitor configuration?"
 
     if [ $? -eq 0 ]; then
         echo "New monitor configuration confirmed. Keeping the changes."
-        # Handle configuration confirmation here
+
+        configure_advanced_settings
     else
         echo "Restoring the old configuration..."
-        cp ~/.config/hypr/conf/monitors/backup ~/.config/hypr/conf/monitors/default.conf
+
+        # Check if there are backup files in the specified folder
+        backup_folder=~/.config/hypr/conf/monitors/backup/
+        if [ -n "$(ls -A $backup_folder)" ]; then
+            cp "$backup_file" ~/.config/hypr/conf/monitors/default.conf
+        else
+            echo "No backup files found in $backup_folder. Skipping restore."
+        fi
+
         gum confirm "Do you want to try configuring the monitor again?"
 
         if [ $? -eq 0 ]; then
@@ -106,6 +219,7 @@ confirm_or_restore_config() {
 
 # Example usage:
 backup_monitor_config
+restore_backup_config
 get_monitor_info
 selected_monitor=$(select_monitor)
 configure_monitors "$selected_monitor"
