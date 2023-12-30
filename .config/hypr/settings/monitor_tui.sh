31f1979920e4219635b7f104c206fa0453ed9e52
Dianaw <65094216+dianaw353@users.noreply.github.com>
2023-12-30 01:50:32 +0000

    some polish

--- .config/hypr/settings/monitor_tui.sh
+++ .config/hypr/settings/monitor_tui.sh
@@ -1,47 +1,116 @@
 #!/bin/bash
-echo "Select a file to load (RETURN = Confirm, ESC = Cancel/Back):"
-# Get the entire output of `hyprctl monitors`
-output=$(hyprctl monitors)
 
-# Count the number of monitors
-count=$(echo "$output" | grep -c 'Monitor')
+figlet "monitor"
 
-# Extract monitor names and IDs
-monitors=$(echo "$output" | awk '/Monitor/ {name=$2} /ID/ {print name, $3}')
+# Function to backup the current monitor configuration
+backup_monitor_config() {
+    echo "Backing up the current monitor configuration..."
+    cp ~/.config/hypr/conf/monitors/default.conf ~/.config/hypr/conf/monitors/backup
+}
 
-# Output the number of monitors and their names
-echo "Currently there are $count monitors connected, their names are: $(echo "$monitors" | awk '{print $1}' | paste -sd ' ')"
+# Function to get information about connected monitors
+get_monitor_info() {
+    output=$(hyprctl monitors)
+    count=$(echo "$output" | grep -c 'Monitor')
+    monitors=$(echo "$output" | awk '/Monitor/ {name=$2} /ID/ {print name, $3}')
 
-# Prompt the user to select their initial screen resolution
-echo "Please select your initial screen resolution. It can be changed later in ~/.config/hypr/config/monitors/monitors.conf"
-echo ""
+    echo "Currently, there are $count monitors connected:"
+    echo "$monitors" | awk '{print "" $1}'
+    echo ""
+}
 
-# Set the same screen resolution for all monitors
-for ((i=1; i<=$count; i++)); do
-    # Get the name of the current monitor
-    monitor_name=$(echo "$monitors" | awk -v i=$i '{if (NR==i) print $1}')
+# Function to select a monitor from the list
+select_monitor() {
+    monitor_list=$(echo "$monitors" | awk '{print $1}' | paste -sd ' ')
+    selected_monitor=$(gum choose --height 15 $monitor_list)
+    echo "$selected_monitor"
+}
 
-    # Display the name of the current monitor
-    echo "Configuring monitor: $monitor_name"
+# Function to configure monitors
+configure_monitors() {
+    selected_monitor=$1
 
-    # Set the screen resolution for the current monitor
-    screenres=$(gum choose --height 15 $(wlr-randr | grep -oP '\d{3,4}x\d{3,4}' | sort -u -n | tac))
-    echo "$screenres"
-    read -e -p "Choose a number to set the scale of the display (default 1): " -i "1" scaling
-    if (( $scaling > '3')); then
-        read -e -p "Unsupported scale, Please choose a number between 1-3: " -i "1" scaling
-    fi
-    echo "$scaling"
-    if grep -q "monitor=$monitor_name,.*,.*,.*" config.conf; then
-        replace=$(echo "monitor=$monitor_name,$screenres,auto,$scaling")
-        sed -i "s/monitor=$monitor_name,.*,.*,.*/$replace/g" ~/.config/hypr/config/monitors/monitors.conf
+    echo "Selected monitor: $selected_monitor"
+
+    # Select screen resolution
+    echo "Select your desired screen resolution for $selected_monitor:"
+    screenres=$(wlr-randr | awk -v selected_monitor="$selected_monitor" '/^'"$selected_monitor"'/,/^$/ {print $1}' | sed '/^$/d' | grep -oP '\d{3,4}x\d{3,4}' | sort -u -n | tac | gum choose --height 15)
+    echo "Setting screen resolution for $selected_monitor to be: $selected_resolution"
+
+    # Select refresh rate
+    echo "Select your desired refresh rate:"
+    refreshrate=$(wlr-randr | awk -v selected_monitor="$selected_monitor" '/^'"$selected_monitor"'/,/^$/ {print $0}' | grep -oP '\d{1,3}\.\d{6}' | awk '{printf("%.0f\n", $1)}' | sort -u -n | tac | gum choose --height 15)
+    echo "You chose the refresh rate to be: $refreshrate"
+
+    # Select position
+    echo "Please select the position of the display relative to other displays"
+    position=$(gum choose --height 15 "auto" "right" "left")
+    echo "Setting position to: $position"
+
+    # Select scale
+    scaling=""
+    while ! [[ "$scaling" =~ ^[0-9]+(\.[0-9]+)?$ ]] || (( $(awk -v x=$scaling -v y=3 'BEGIN {print (x > y)}') )); do
+        echo "Select your desired scale for your screen (1-3, default 1):"
+        scaling=$(gum input --placeholder "Enter the scale (1-3, default 1): ")
+        scaling=${scaling:-1}  # Set default to 1 if the input is empty or invalid
+    done
+    echo "Setting scale to: $scaling"
+
+    # Extract x and y resolutions
+    x_resolution=$(echo "$screenres" | cut -d'x' -f1)
+    y_resolution=$(echo "$screenres" | cut -d'x' -f2)
+    
+    case $position in
+    "auto")
+        position_res="auto"
+        ;;
+    "right")
+        position_res="${x_resolution}x0"
+        ;;
+    "left")
+        position_res="-${x_resolution}x0"
+        ;;
+    *)
+        echo "Invalid position. Defaulting to top-right."
+        position_res="${x_resolution}x0"
+        ;;
+    esac
+
+     # Replace or add the line in the default.conf file
+    if grep -q "monitor=$selected_monitor" ~/.config/hypr/conf/monitors/default.conf; then
+        sed -i "s/monitor=$selected_monitor,.*/monitor=$selected_monitor,$screenres@$refreshrate,$position_res,$scaling/g" ~/.config/hypr/conf/monitors/default.conf
     else
-        echo "monitor=$monitor_name,$screenres,auto,$scaling" >> ~/.config/hypr/config/monitors/monitors.conf
+        echo "monitor=$selected_monitor,$screenres@$refreshrate,auto,$scaling" >> ~/.config/hypr/conf/monitors/default.conf
     fi
-    swww init
-done
+}
 
-echo "Initial screen resolution set to $screenres"
+# Function to confirm or restore the monitor configuration
+confirm_or_restore_config() {
+    gum confirm "Do you want to use the new monitor configuration?"
+
+    if [ $? -eq 0 ]; then
+        echo "New monitor configuration confirmed. Keeping the changes."
+        # Handle configuration confirmation here
+    else
+        echo "Restoring the old configuration..."
+        cp ~/.config/hypr/conf/monitors/backup ~/.config/hypr/conf/monitors/default.conf
+        gum confirm "Do you want to try configuring the monitor again?"
+
+        if [ $? -eq 0 ]; then
+            selected_monitor=$(select_monitor)
+            configure_monitors "$selected_monitor"
+            confirm_or_restore_config
+        fi
+    fi
+}
+
+# Example usage:
+backup_monitor_config
+get_monitor_info
+selected_monitor=$(select_monitor)
+configure_monitors "$selected_monitor"
+confirm_or_restore_config
+
+echo "Initial screen resolution set to $selected_resolution"
 echo "The display port that has been configured is: $(wlr-randr | grep -w connected | awk '{print $1}')"
 echo "No more screens left to configure."
-
